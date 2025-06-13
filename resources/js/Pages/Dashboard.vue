a355ed0067713f7b762d041d85d4e80183186b71
alejandro <jorgerenteriareyes4@gmail.com>
2025-06-12 19:05:04 -0600

    cambios en el dashboard y authenticades layot

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -11,7 +11,9 @@
         <div class="py-4">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 border-b border-gray-200 font-semibold text-2xl">
+                    <div
+                        class="p-6 text-gray-900 border-b border-gray-200 font-semibold text-2xl"
+                    >
                         Bienvenido a batalla naval!
                     </div>
 
@@ -24,29 +26,66 @@
                     </div>
 
                     <div class="p-8 text-gray-900">
-                        <PrimaryButton class="mr-4" @click="$inertia.visit('/games/create')">
+                        <PrimaryButton class="mr-4" @click="openModal">
                             Crear nueva partida
                         </PrimaryButton>
-                        <PrimaryButton @click="$inertia.visit('/games/join')" class="ml-4">
+                        <PrimaryButton class="ml-4">
                             Unirse a una partida
                         </PrimaryButton>
                     </div>
                 </div>
             </div>
+
+            <Modal @close="showModal = false" v-if="showModal">
+                <template #title>
+                    <h3 class="text-lg leading-6 font-medium text-gray-900">
+                        Crea una nueva partida
+                    </h3>
+                </template>
+                <template #body>
+                    <form @submit.prevent="crearPartida">
+                        <input
+                            v-model="nombrePartida"
+                            type="text"
+                            placeholder="Nombre"
+                            class="border p-2 w-full mb-4 rounded"
+                        />
+                        <PrimaryButton type="submit">Crear</PrimaryButton>
+                    </form>
+                </template>
+            </Modal>
         </div>
     </AuthenticatedLayout>
 </template>
 
 <script>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import  PrimaryButton  from "@/Components/PrimaryButton.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import Modal from "@/Components/Modal.vue";
 import { Head } from "@inertiajs/vue3";
 
 export default {
     components: {
         AuthenticatedLayout,
         Head,
-        PrimaryButton
+        PrimaryButton,
+        Modal,
+    },
+
+    data() {
+        return {
+            showModal: false,
+            nombrePartida: '',
+        };
+    },
+    methods: {
+        openModal() {
+            this.showModal = true;
+        },
+        crearPartida() {
+            console.log('Creando partida:', this.nombrePartida);
+            this.showModal = false;
+        }
     },
 };
 </script>
